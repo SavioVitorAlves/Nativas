69521bbca31bdfad22e3b180e3d2cb169b977631
SavioVitorAlves <saviovitor2@gmail.com>
2024-06-27 11:11:06 -0300

    feat: add new funcion of sales

--- src/components/Layout/main/SalesArea.jsx
+++ src/components/Layout/main/SalesArea.jsx
@@ -1,9 +1,17 @@
 import style from './SalesArea.module.css'
+import variedades from '../../img/variedades.png'
+import jatai from '../../img/abelha.png'
 function SalesArea(){
     return (
-        <header className={style.salesarea}>
-            SalesArea
-        </header>
+        <section className={style.salesarea}>
+            <img src={variedades} alt="variedades de produtos de abelhas sem ferrão" />
+            <div>
+                <h1>Mel de Uruçu-Amarela</h1>
+                <h3>mais de 12 tipos de<br/>beneficios diferentes</h3>
+                <button className={style.button}>adquira o seu</button>
+            </div>
+            <img className={style.jatai} src={jatai} alt="" />
+        </section>
     )
 }
 export default SalesArea
