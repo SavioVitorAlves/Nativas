f62071b4c29ca76bfb2e7a5cd831887a3f1dd60f
SavioVitorAlves <saviovitor2@gmail.com>
2024-06-27 11:09:06 -0300

    docs: add new view creator

--- src/components/Layout/main/Creator.jsx
+++ src/components/Layout/main/Creator.jsx
@@ -2,7 +2,18 @@ import style from './Creator.module.css'
 function Creator(){
     return (
         <header className={style.creator}>
-            creator
+            <div className={style.name}>
+                <h2>Nome <br/> Principal</h2>
+                <p>Formado em <br/> Apicultura UFNT</p>
+            </div>
+            <div className={style.spec}>
+                <header>
+                    apicultor, professor,<br/> pecuarista e empresario
+                </header>
+                <h5>
+                    bslfçsdknfssndfsçlkdn
+                </h5>
+            </div>
         </header>
     )
 }
