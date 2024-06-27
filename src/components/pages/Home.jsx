b852190bc2201ce280a2ff9b4ea44fc6dc680f31
SavioVitorAlves <saviovitor2@gmail.com>
2024-06-27 11:10:29 -0300

    feat: add new color in backgroud

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,11 +3,13 @@ import Benefict from '../Layout/main/Benefict';
 import SalesArea from '../Layout/main/SalesArea';
 import Cards from '../Layout/main/Cards';
 import Products from '../Layout/main/Products';
+import Creator from '../Layout/main/Creator';
 function Home(){
     return (
         <div>
             <Header/>
             <Benefict/>
+            <Creator/>
             <SalesArea/>
             <Cards/>
             <Products/>
