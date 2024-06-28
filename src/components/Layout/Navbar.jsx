7f2fc6f9a0ad39ed3efb0f0fa052908f528e5ac5
SavioVitorAlves <saviovitor2@gmail.com>
2024-06-28 17:17:59 -0300

    feat: add links of rotes

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,5 +1,33 @@
 import style from './Navbar.module.css'
+import {Link} from 'react-router-dom'
+import Container from'./Container'
 function Navbar(){
-    return <p className={style.navbar}>Navbar</p>
+    return( 
+        <Container>
+            <nav className={style.navbar}>
+                <div>
+                    <Link to="/">
+                        LOGO
+                    </Link>
+                    
+                </div>
+                <ul>
+                    <li>
+                        <Link to="/envio">Envio</Link>
+                    </li>
+                    <li>
+                        <a href="#Benefict">Beneficios</a> 
+                    </li>
+                    <li>
+                        <a href="#Sobre">Sobre</a> 
+                        
+                    </li>
+                    <li>
+                        <a href="#Produtos">Produtos</a> 
+                    </li>
+                </ul>
+            </nav>
+        </Container>
+    )
 }
 export default Navbar
