91e8201b085e03640061980dc2f5c8052cf21f4d
SavioVitorAlves <saviovitor2@gmail.com>
2024-06-28 17:18:25 -0300

    refactor: add new text

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,50 @@
 import style from './Footer.module.css'
+import masatercard from '../img/mastercard.png'
+import pagbank from '../img/pagbank.png'
+import visa from '../img/visa.jpeg'
 function Footer(){
-    return <p className={style.footer}>footer</p>
+    return( 
+        <footer className={style.footer}>
+            <div className={style.info}>
+                <div>
+                    <h6>Institucional</h6>
+                    <div>
+                        <p>saviovitor2@gmail.com</p>
+                        <p>(63) 99108-0174</p>
+                    </div>
+                </div>
+                <div>
+                    <h6>Contato</h6>
+                    <div>
+                        <p>saviovitor2@gmail.com</p>
+                        <p>(63) 99108-0174</p>
+                    </div>
+                </div>
+                <div>
+                    <h6>Social</h6>
+                    <div>
+                        <p>saviovitor2@gmail.com</p>
+                        <p>(63) 99108-0174</p>
+                    </div>
+                </div>
+                <div>
+                    <h6>Localização</h6>
+                    <div>
+                        <p>saviovitor2@gmail.com</p>
+                        <p>(63) 99108-0174</p>
+                    </div>
+                </div>
+            </div>
+            <div className={style.cartoes}>
+                <p>Nativas &copy; 2024</p>
+                <div className={style.img}>
+                    <img src={masatercard} alt="" />
+                    <img src={pagbank} alt="" />
+                    <img src={visa} alt="" /> 
+                </div>
+            </div>
+        </footer>
+    )
+
 }
 export default Footer
