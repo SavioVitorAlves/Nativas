ac823ae496f3fa93cfd6b62c0d76932379d7ba08
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-01 18:29:09 -0300

    docs: creat and configurate

---
+++ src/components/Layout/Shipping/Envio.jsx
@@ -0,0 +1,9 @@
+import sytle from './Envio.module.css'
+function Envio(){
+    return (
+        <div className={sytle.envio}>
+            <p>verifique o envio do seu produto aqui</p>
+        </div>
+    )
+}
+export default Envio
