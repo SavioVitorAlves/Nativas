21475623e7e167f9680230599e0351152fde5490
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-04 11:01:21 -0300

    refactor: add imgs

--- src/components/Layout/main/Benefict.jsx
+++ src/components/Layout/main/Benefict.jsx
@@ -1,9 +1,14 @@
 import style from './Benefict.module.css'
+import açucar from '../../img/img-icons/açucar.png'
+import antioxidante from '../../img/img-icons/antioxidante.png'
+import digestao from '../../img/img-icons/digestão.png'
+import minerais from '../../img/img-icons/minerais.png'
 function Benefict(){
     return (
         <div id="Benefict" className={style.benefict}>
             <div className={style.ben_item}>
                 <div className={style.cicle}>
+                    <img src={antioxidante} alt="Produtos de abelhas sem ferrão com antioxidantes"/>
                 </div>
                 <h4>Antioxidantes</h4>
                 <p>Rico em antioxidantes, que ajudam a combater os radicais livres 
@@ -11,21 +16,24 @@ function Benefict(){
            </div>
            
            <div className={style.ben_item}>
-           <div className={style.cicle}>
+                <div className={style.cicle}>
+                    <img src={minerais} alt="Produtos de abelhas sem ferrão com antioxidantes"/>
                 </div>
                 <h4>Vitaminas e Minerais</h4>
                 <p>Contém uma variedade de vitaminas (como a vitamina C e B) 
                 e minerais (como o cálcio, ferro, potássio e magnésio).</p>
            </div>
            <div className={style.ben_item}>
-           <div className={style.cicle}>
+                <div className={style.cicle}>
+                    <img src={digestao} alt="Produtos de abelhas sem ferrão com antioxidantes"/>
                 </div>
                 <h4>Facilita a Digestão</h4>
                 <p>Pode ser mais fácil de digerir do que o mel convencional,
                 sendo adequado para pessoas com problemas digestivos.</p>
            </div>
            <div className={style.ben_item}>
-           <div className={style.cicle}>
+                <div className={style.cicle}>
+                    <img src={açucar} alt="Produtos de abelhas sem ferrão com antioxidantes"/>
                 </div>
                 <h4>Controle de Açúcar</h4>
                 <p>Possui índice glicêmico mais baixo do que de abelhas com ferrão,
--- src/components/Layout/main/Cards.jsx
+++ src/components/Layout/main/Cards.jsx
@@ -1,6 +1,16 @@
 
 import style from './Cards.module.css';
+// --------importando imagens ----------
 import mel from '../../img/mel.png';
+import ana_sousa from '../../img/img-pessoas/ana_sousa.png';
+import beatriz_ferreira from '../../img/img-pessoas/beatriz_ferreira.png';
+import carlos_lima from '../../img/img-pessoas/carlos_lima.png';
+import felipe_martins from '../../img/img-pessoas/felipe_martins.png';
+import joao_pereira from '../../img/img-pessoas/joao_pereira.png';
+import luciana_almeida from '../../img/img-pessoas/luciana_almeida.png';
+import mariana_santos from '../../img/img-pessoas/mariana_santos.png';
+import rafael_oliveira from '../../img/img-pessoas/rafael_oliveira.png';
+//----------pluguin do carrocel---------
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -19,7 +29,7 @@ function Cards() {
             <div className={style.carousel} /*ref={carouselRef}*/>
                <Slider {...settings}>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={ana_sousa} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Ana Souza</h5>
                     <p>Comprei o mel de abelhas sem ferrão e fiquei maravilhada com o sabor. Além disso, senti uma melhora 
                     na minha digestão e disposição diária. Recomendo a todos!</p>
@@ -32,7 +42,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={carlos_lima} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Carlos Lima</h5>
                     <p>O própolis de abelhas sem ferrão que comprei ajudou muito na minha imunidade. Desde que comecei a usar,
                     não peguei mais resfriados frequentes. Excelente produto!</p>
@@ -45,7 +55,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={mariana_santos} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Mariana Santos</h5>
                     <p>Uso o mel de abelhas sem ferrão na minha rotina de cuidados com a pele e percebi uma grande diferença na
                     hidratação e cicatrização de pequenas feridas. É realmente incrível!</p>
@@ -58,7 +68,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={rafael_oliveira} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Rafael Oliveira</h5>
                     <p>Minha esposa e eu adoramos o sabor único do mel de abelhas sem ferrão. Além disso, sentimos que nosso 
                     sistema imunológico está mais forte desde que começamos a consumir regularmente.</p>
@@ -71,7 +81,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={beatriz_ferreira} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Beatriz Ferreira</h5>
                     <p>Depois de começar a usar os produtos de abelhas sem ferrão, minha energia aumentou e a saúde intestinal 
                     melhorou significativamente. Não troco por nada!</p>
@@ -84,7 +94,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={joao_pereira} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>João Pereira</h5>
                     <p>Sofria com problemas digestivos e o mel de abelhas sem ferrão foi uma solução natural e eficaz. 
                     Melhora minha digestão e ainda tem um sabor delicioso.</p>
@@ -97,7 +107,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={luciana_almeida} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Luciana Almeida</h5>
                     <p>O pólen de abelhas sem ferrão que comprei trouxe muitos benefícios para minha saúde. Notei um aumento
                     na minha disposição e energia ao longo do dia.</p>
@@ -110,7 +120,7 @@ function Cards() {
                     </div>
                 </div>
                 <div className={style.testemunho}>
-                    <div className={style.cicle}></div>
+                    <div className={style.cicle}><img src={felipe_martins} alt="clientes que compraram meu de abelha sem ferrão" /></div>
                     <h5>Felipe Martins</h5>
                     <p>Sou apaixonado por produtos naturais e o mel de abelhas sem ferrão superou minhas expectativas. Sinto-me
                     mais saudável e com mais energia desde que comecei a usar.</p>
