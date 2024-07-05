79978a1e3b4a49f33dc1a9a3ca8f8c5f1999dfea
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-05 17:58:53 -0300

    feat: add new img

--- src/components/Layout/main/Products.jsx
+++ src/components/Layout/main/Products.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import style from './Products.module.css'
+// ----------importando as imagens -------------
 import mel from '../../img/img-products/mel.jpg'
+import atrativo from '../../img/img-products/atrativo.png'
+import bombom_de_polem from '../../img/img-products/bombom_de_polem.jpg'
+import caixa from '../../img/img-products/caixa.jpg'
+import mel_uruçu from '../../img/img-products/mel_uruçu.jpg'
+import propolis from '../../img/img-products/propolis.jpeg'
+
 import comeia from '../../img/comeia.png'
 import Card from '../../cards/Card'
 
@@ -10,35 +17,35 @@ function Products(){
             <header className={style.header}>  Nossos Produtos</header>
             <section className={style.grid}>
                 <Card
-                    img={mel}
-                    nome="Nome do Produto"
-                    preco="49,50"
+                    img={mel_uruçu}
+                    nome="Mel de Uruçu-Amarela"
+                    preco="24,99"
                     
                 />
                 <Card
-                    img={mel}
-                    nome="Nome do Produt"
+                    img={atrativo}
+                    nome="Atrativos Para Capitura de Abelhas sem Ferrão"
+                    preco="19,66"
+                />
+                <Card
+                    img={bombom_de_polem}
+                    nome="Bom Bom de Polem"
+                    preco="39,90"
+                />
+                <Card
+                    img={caixa}
+                    nome="Caixa de Criação"
                     preco="49,50"
                 />
                 <Card
-                    img={mel}
-                    nome="Nome do Produto"
-                    preco="89,50"
+                    img={propolis}
+                    nome="Propolis de Abelhas Nativas"
+                    preco="45,50"
                 />
                 <Card
                     img={mel}
-                    nome="Nome do Produto"
-                    preco="49,50"
-                />
-                <Card
-                    img={mel}
-                    nome="Nome do Produto"
-                    preco="49,50"
-                />
-                <Card
-                    img={mel}
-                    nome="Nome do Produto"
-                    preco="49,50"
+                    nome="Mel de Jatai 50ml"
+                    preco="29,99"
                 />
             </section>
             <img className={style.img} src={comeia} alt="caomeia de abelhas sem ferrão" />
