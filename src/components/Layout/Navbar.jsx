efa7a190c4317145bf15b3427465db63890c2a18
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-06 11:12:36 -0300

    feat: responsividade of cod

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,7 +1,20 @@
 import style from './Navbar.module.css'
 import {Link} from 'react-router-dom'
 import Container from'./Container'
+import React, { useRef } from 'react';
 function Navbar(){
+    
+    const menuRef = useRef(null);
+    const listaRef = useRef(null);
+
+    function ativaMenu() {
+        if (listaRef.current.style.display === 'none') {
+            listaRef.current.style.display = 'block';
+          } else {
+            listaRef.current.style.display = 'none';
+          }          
+    }
+    
     return( 
         <Container>
             <nav className={style.navbar}>
@@ -11,6 +24,7 @@ function Navbar(){
                     </Link>
                     
                 </div>
+                <span className="material-symbols-outlined" ref={menuRef} onClick={ativaMenu}>menu</span>
                 <ul>
                     <li>
                         <Link to="/shipping">Envio</Link>
@@ -27,7 +41,17 @@ function Navbar(){
                     </li>
                 </ul>
             </nav>
+            <div className={style.menu_mobile} ref={listaRef} style={{ display: 'none' }}>
+                <ul >
+                    <li><a href="index.html#cabecalho">Home</a></li>
+                    <li><a href="index.html#footer">Contato</a></li>
+                    <li><a href="index.html#mapa">Localização</a></li>
+                    <li><a href="index.html#historia">Sobre</a></li>
+                    <li><a href="">Institucional</a></li>
+                </ul>
+            </div>
         </Container>
     )
+
 }
 export default Navbar
